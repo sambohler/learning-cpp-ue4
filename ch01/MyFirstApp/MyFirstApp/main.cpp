e13770299c7ef3bb0d97920a0570ede6c890bd07
Sam Bohler <sam@bitpay.com>
2015-06-20 06:43:46 -0500

    Chapter 01: Creating your first C++ program

---
+++ ch01/MyFirstApp/MyFirstApp/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  MyFirstApp
+//
+//  Created by Samuel Bohler on 6/20/15.
+//  Copyright (c) 2015 Samuel Bohler. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, world" << endl;
+    cout << "I am now a C++ programmer." << endl;
+    return 0;
+}
