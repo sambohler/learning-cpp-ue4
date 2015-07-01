eeec2aef247ae7eec6218f2011fb666460e3c34c
Sam Bohler <sam@bitpay.com>
2015-07-01 07:19:52 -0400

    Chapter 03: Puzzle Game

---
+++ ch03/Puzzle/Source/Puzzle/PuzzlePlayerController.cpp
@@ -0,0 +1,12 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "Puzzle.h"
+#include "PuzzlePlayerController.h"
+
+APuzzlePlayerController::APuzzlePlayerController()
+{
+	bShowMouseCursor = true;
+	bEnableClickEvents = true;
+	bEnableTouchEvents = true;
+	DefaultMouseCursor = EMouseCursor::Crosshairs;
+}
