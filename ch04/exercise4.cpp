3a518c64008e79e2f2aa0a5ee7b34430f6960c7b
Sam Bohler <sam@bitpay.com>
2015-07-01 07:31:55 -0400

    Chapter 04: Exercises 1-4

---
+++ ch04/exercise4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    while (i <= 16)
+    {
+        cout << i << "\t" << i*i << endl;
+        i++;
+    }
+    return 0;
+}
